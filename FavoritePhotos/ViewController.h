fbe621fb23d3540e20907b3cc4c3f943480c8d7c
Tewodros Wondimu <tewodros.wr@gmail.com>
2015-01-22 16:26:24 -0800

    Initial Commit

---
+++ FavoritePhotos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FavoritePhotos
+//
+//  Created by Tewodros Wondimu on 1/22/15.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
